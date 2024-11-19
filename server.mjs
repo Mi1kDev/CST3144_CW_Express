a5d612775a43ea2a09be4f58844d62069270341a
Mi1kDev <tjthemorgan@gmail.com>
2024-11-19 19:34:00 +0000

    Added cors options

--- server.mjs
+++ server.mjs
@@ -22,7 +22,14 @@ app.use(bodyParser.json())
 app.use((morgan('tiny')))
 
 // middleware to allow cross reference communication or communication across two different domains
-app.use(cors())
+const corsOptions = {origin: "*",   
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",   
+    allowedHeaders: "Access-Control-Allow-Headers,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers,Origin,Cache-Control,Content-Type,X-Token,X-Refresh-Token",  
+    credentials: true,   
+    preflightContinue: false,  
+    optionsSuccessStatus: 204 
+};
+app.use(cors(corsOptions))
 
 //images folder made available to public
 app.use(express.static("images"))
