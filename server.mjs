cfcfa86389aaf2f077be916e085d699ffc7b379a
Mi1kDev <tjthemorgan@gmail.com>
2024-11-19 19:50:29 +0000

    Cors adjustments

--- server.mjs
+++ server.mjs
@@ -21,18 +21,12 @@ app.use(bodyParser.json())
 // middleware to log the various connections and communications to and from the server
 app.use((morgan('tiny')))
 
+// middleware to allow cross reference communication or communication across two different domains
+app.use(cors)
+
 //images folder made available to public
 app.use(express.static("images"))
 
-// middleware to allow cross reference communication or communication across two different domains
-const corsOptions = {origin: "*",   
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",   
-    allowedHeaders: "Access-Control-Allow-Headers,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers,Origin,Cache-Control,Content-Type,X-Token,X-Refresh-Token",  
-    credentials: true,   
-    preflightContinue: false,  
-    optionsSuccessStatus: 204 
-};
-app.use(cors(corsOptions))
 
 // api endpoint to return all lessons in the database to the requester
 app.get("/lessons", (req, res)=>{
